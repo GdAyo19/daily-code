97c3b9091f9e1be55c04cf6ec9a5c6d63d123866
aolanrew <theygolarewaju@gmail.com>
2026-06-26 08:37:30 +0100

    currently working on it

---
+++ asciiWorks/asciiJustify.go
@@ -0,0 +1,24 @@
+package main
+
+// func AsciiJustify(text, banner string) string {
+// 	banner = banner + ".txt"
+// 	data, err := os.ReadFile(banner)
+// 	if err != nil {
+// 		fmt.Println("Unable to read file")
+// 		return ""
+// 	}
+
+// 	line := strings.Split(string(data), "\n")
+
+// 	text = strings.ReplaceAll(text, "\r\n", "\n")
+// 	text = strings.ReplaceAll(text, "\\n", "\n")
+// 	words := strings.Split(text, "\n")
+// 	fmt.Println(words)
+// 	result := ""
+
+// 	for _, word := range words {
+
+// 	}
+
+// 	return result
+// }
